bd05077fe56d1b50e431dc0aead74000e902439a
Vinicius Hansel <hanelvinicius@gmail.com>
2025-10-01 21:50:45 -0300

    fix: add requestMapping and responseEntity

--- infrastructure/src/main/kotlin/com/henriquebarucco/ssbc/entrypoint/http/v1/sensor/GetSensorByIdController.kt
+++ infrastructure/src/main/kotlin/com/henriquebarucco/ssbc/entrypoint/http/v1/sensor/GetSensorByIdController.kt
@@ -1,11 +1,15 @@
 package com.henriquebarucco.ssbc.entrypoint.http.v1.sensor
 
-import com.henriquebarucco.ssbc.sensor.get.dto.GetSensorByIdOutput
+import com.henriquebarucco.ssbc.entrypoint.http.v1.sensor.dto.GetSensorByIdResponse
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.GetMapping
 import org.springframework.web.bind.annotation.PathVariable
+import org.springframework.web.bind.annotation.RequestMapping
 
+@RequestMapping("/v1/sensors")
 interface GetSensorByIdController {
-
     @GetMapping("/sensors/{id}")
-    fun getSensorById(@PathVariable id: String) : GetSensorByIdOutput
+    fun getSensorById(
+        @PathVariable id: String,
+    ): ResponseEntity<GetSensorByIdResponse>
 }
--- infrastructure/src/main/kotlin/com/henriquebarucco/ssbc/entrypoint/http/v1/sensor/impl/GetSensorByIdControllerImpl.kt
+++ infrastructure/src/main/kotlin/com/henriquebarucco/ssbc/entrypoint/http/v1/sensor/impl/GetSensorByIdControllerImpl.kt
@@ -1,13 +1,17 @@
 package com.henriquebarucco.ssbc.entrypoint.http.v1.sensor.impl
 
 import com.henriquebarucco.ssbc.entrypoint.http.v1.sensor.GetSensorByIdController
+import com.henriquebarucco.ssbc.entrypoint.http.v1.sensor.dto.GetSensorByIdResponse
 import com.henriquebarucco.ssbc.sensor.get.GetSensorByIdUseCase
-import com.henriquebarucco.ssbc.sensor.get.dto.GetSensorByIdOutput
+import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.RestController
 
 @RestController
 class GetSensorByIdControllerImpl(
     private val getSensorBydIdUseCase: GetSensorByIdUseCase,
 ) : GetSensorByIdController {
-    override fun getSensorById(id: String): GetSensorByIdOutput = getSensorBydIdUseCase.execute(id)
+    override fun getSensorById(id: String): ResponseEntity<GetSensorByIdResponse> {
+        val fromOutput = GetSensorByIdResponse.fromOutput(getSensorBydIdUseCase.execute(id))
+        return ResponseEntity.ok(fromOutput)
+    }
 }
