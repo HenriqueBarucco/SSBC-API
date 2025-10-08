675bbf2ebd3aebc40be6691525541715e5fb41d4
Henrique Barucco <henrique_barucco@hotmail.com>
2025-10-08 12:57:10 -0300

    fix: use feign formdata

--- infrastructure/src/main/kotlin/com/henriquebarucco/ssbc/configuration/FeignConfig.kt
+++ infrastructure/src/main/kotlin/com/henriquebarucco/ssbc/configuration/FeignConfig.kt
@@ -2,11 +2,15 @@ package com.henriquebarucco.ssbc.configuration
 
 import feign.codec.Encoder
 import feign.form.spring.SpringFormEncoder
+import org.springframework.beans.factory.ObjectFactory
+import org.springframework.boot.http.converter.autoconfigure.HttpMessageConverters
+import org.springframework.cloud.openfeign.support.SpringEncoder
 import org.springframework.context.annotation.Bean
 import org.springframework.context.annotation.Configuration
 
 @Configuration
 class FeignConfig {
     @Bean
-    fun encoder(): Encoder = SpringFormEncoder()
+    fun feignFormEncoder(messageConverters: ObjectFactory<HttpMessageConverters>): Encoder =
+        SpringFormEncoder(SpringEncoder(messageConverters))
 }
--- infrastructure/src/main/kotlin/com/henriquebarucco/ssbc/external/easywhatsapp/EasyWhatsAppClient.kt
+++ infrastructure/src/main/kotlin/com/henriquebarucco/ssbc/external/easywhatsapp/EasyWhatsAppClient.kt
@@ -2,6 +2,7 @@ package com.henriquebarucco.ssbc.external.easywhatsapp
 
 import com.henriquebarucco.ssbc.external.easywhatsapp.dto.SendTextMessageRequest
 import org.springframework.cloud.openfeign.FeignClient
+import org.springframework.http.MediaType
 import org.springframework.http.ResponseEntity
 import org.springframework.web.bind.annotation.PostMapping
 import org.springframework.web.bind.annotation.RequestBody
@@ -10,12 +11,15 @@ import org.springframework.web.multipart.MultipartFile
 
 @FeignClient(name = "easy-whatsapp-api-client", url = "\${easy-whatsapp.url}")
 interface EasyWhatsAppClient {
-    @PostMapping("\${easy-whatsapp.endpoints.text-message}")
+    @PostMapping(value = ["\${easy-whatsapp.endpoints.text-message}"], consumes = [MediaType.APPLICATION_JSON_VALUE])
     fun sendTextMessage(
         @RequestBody request: SendTextMessageRequest,
     ): ResponseEntity<*>
 
-    @PostMapping("\${easy-whatsapp.endpoints.image-message}")
+    @PostMapping(
+        value = ["\${easy-whatsapp.endpoints.image-message}"],
+        consumes = [MediaType.MULTIPART_FORM_DATA_VALUE],
+    )
     fun sendImageMessage(
         @RequestPart("token") token: String,
         @RequestPart("phone") phone: String,
